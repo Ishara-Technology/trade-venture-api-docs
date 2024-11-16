b5ff7621b56a695ca85f24e889c1255fc47dfde3
Shahjalal <mdshahjalal.bu@gmail.com>
2024-11-16 22:01:00 +0600

    config file change

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
-
-
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
 })
 
-const isProduction = process.env.NODE_ENV === 'production'
-
-module.exports = withNextra({
-  basePath: isProduction ? '/trade-venture-api-docs' : '', // Replace with your repo name
-  assetPrefix: isProduction ? '/trade-venture-api-docs/' : '', // Replace with your repo name
-})
+module.exports = withNextra()
